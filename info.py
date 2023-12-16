c5d301b9845a93102d5db12dd66261ec9e7257e8
Abujigamer <153810835+Abujigamer@users.noreply.github.com>
2023-12-16 17:36:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://yogeshpandit253642:Mw99ngQsePS0doAj@cluster0.2l4gz99.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mohdaltamash4949:<mohdaltamash4949>@cluster0.eed6kbq.mongodb.net/")
 DATABASE_NAME = environ.get('DATABASE_NAME', "vkbotsthalpathy")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
