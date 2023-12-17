6b5d166a48247999dce2e45aeef32f75d90a7f12
Abujigamer <153810835+Abujigamer@users.noreply.github.com>
2023-12-17 19:20:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -30,11 +30,11 @@ async def start(client, message):
         buttons = [[
     [InlineKeyboardButton('🔰 Add Me To Your Group 🔰', url=f'http://t.me/{temp.U_NAME}?startgroup=true'),
     ]]
-            InlineKeyboardButton('🔱 Support Group 🔱', url="https://t.me/+AY5o4Wx52KQ1NjRl")
-                    InlineKeyboardButton('🌿 Movie Group 🌿', url='https://t.me/Favouritemoves')
-                ],[
-                  InlineKeyboardButton('⚜️ Update channel ⚜️', url='https://t.me/Abuji_update')
-        ]]
+      InlineKeyboardButton('🔱 Support Group 🔱', url="https://t.me/+AY5o4Wx52KQ1NjRl")
+      InlineKeyboardButton('🌿 Movie Group 🌿', url='https://t.me/Favouritemoves')
+    ],[
+       InlineKeyboardButton('⚜️ Update channel ⚜️', url='https://t.me/Abuji_update')
+      ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, disable_web_page_preview=True)
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
