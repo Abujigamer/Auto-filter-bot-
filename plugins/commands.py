029c7ba656634dd861a4719ef358ba07018dac19
Abujigamer <153810835+Abujigamer@users.noreply.github.com>
2023-12-18 18:39:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -902,7 +902,7 @@ async def requests(bot, message):
               ]]
         await message.reply_text("<b>Your request has been added! Please wait for some time.\n\nJoin Channel First & View Request</b>", reply_markup=InlineKeyboardMarkup(btn))
     
-@Client.on_message(filters.command("send") & filters.user(ADMINS)
+@Client.on_message(filters.command("send") & filters.user(ADMINS))
 async def send_msg(bot, message):
     if message.reply_to_message:
         target_id = message.text.split(" ", 1)[1]
